406ce0b13c4fa94450b0b74c0fca723c1a4ba4b6
siddharth <ssiddharth014@gmail.com>
2020-03-22 22:18:14 +0530

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const cookieParser=require('cookie-parser');
 //const path=require('path');
-const port=8000;
+const port=process.env.PORT|| 8000;
 const app=express();
 //layout
 const expressLayouts= require('express-ejs-layouts');
