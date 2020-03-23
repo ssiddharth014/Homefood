2984a4a1b2fa189b6ba013a6ada7571589fb7d84
siddharth <ssiddharth014@gmail.com>
2020-03-23 21:46:50 +0530

    unified

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const URL="mongodb+srv://dbuser:dbuser@cluster0-w0xwb.mongodb.net/test?retryWrites=true&w=majority";
 
 const db=async() =>{
-await mongoose.connect(URL,{userUnifiedTopology:true,useNewUrlParser:true})
+await mongoose.connect(URL,{useUnifiedTopology:true,useNewUrlParser:true})
 console.log('connected..');
 }
 
