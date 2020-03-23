1e246efe38bdd1a0f1c09bb600d8f7d702e19918
siddharth <ssiddharth014@gmail.com>
2020-03-23 21:26:28 +0530

    changed

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,5 @@
  const mongoose= require('mongoose');
 const URL="mongodb+srv://dbuser:dbuser@cluster0-w0xwb.mongodb.net/test?retryWrites=true&w=majority";
-mongoose.connect('mongodb://localhost/social-house__development');
 
 const db=async() =>{
 await mongoose.connect(URL,{userUnifiedTopology:true,useNewUrlParser:true})
@@ -12,16 +11,3 @@ console.log('connected..');
 module.exports=db;
 
 
-
-/*
-
-const MongoClient = require('mongoose');
-const uri = "mongodb+srv://dbuser:dbuser@cluster0-w0xwb.mongodb.net/test?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-module.exports=client;
-*/
